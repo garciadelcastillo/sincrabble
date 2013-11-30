225596c20c490981e78e4aec37762aacee92e81f
garciadelcastillo <personal@garciadelcastillo.es>
2013-11-30 18:18:42 -0500

    Attempt to optimize, not very successful...

--- server/helpers.rb
+++ server/helpers.rb
@@ -7,19 +7,57 @@ helpers do
 
 		# generate all posible permutations of the given letters
 		# --> this block gets really expensive beyond 9 chars, should be optimized
+		puts "DEBUG: Calculating permutations with RUBY PERMUTATION method: "
 		perm = {} 
-		for i in 1.._query.length
-			letters.permutation(i).each do |word|
-				perm[word.join] = true
+		puts Benchmark.measure {
+			for i in 1..clean_query.length
+				letters.permutation(i).each do |word|
+					perm[word.join] = true
+				end
 			end
-		end
+		}
 
 		# parse the dictionary file for correct english words
+		puts "DEBUG: Performing HASH LOOKUP: "
 		words = [] 
-		File.open('./assets/words.txt').each_line do |line|
-			line.chomp!
-			words << line if perm[line]
-		end
+		puts Benchmark.measure {
+			File.open('./assets/words.txt').each_line do |line|
+				line.chomp!
+				words << line if perm[line]
+			end
+		}
+
+		# AN ATTEMPT TO MAKE IT FASTER, ONLY REDUCED TIME TO AROUND 1/2
+
+		# puts "DEBUG: Calculating permutations with CUSTOM PERMUTE method: "
+		# @perm = {}
+		# puts Benchmark.measure {
+
+		# 	def permute(_base, _letters, _len) 
+		# 		len = _len - 1
+
+		# 		for i in 0..len
+		# 			s = _base + _letters[i]
+		# 			@perm[s] = true
+		# 			_lett = Array.new(_letters)
+		# 			_lett.delete_at(i)
+		# 			permute(s, _lett, len)
+		# 		end
+
+		# 	end
+			
+		# 	permute('', letters, letters.length)
+		# }
+
+		# # parse the dictionary file for correct english words
+		# puts "DEBUG: Performing HASH LOOKUP: "
+		# words = [] 
+		# puts Benchmark.measure {
+		# 	File.open('./assets/words.txt').each_line do |line|
+		# 		line.chomp!
+		# 		words << line if @perm[line]
+		# 	end
+		# }
 
 		# calculate scrabble score for each word
 		scores = {}

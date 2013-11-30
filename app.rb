2e88ea4754dc678ee8d89543418957b2902d6687
garciadelcastillo <personal@garciadelcastillo.es>
2013-11-29 19:37:23 -0500

    Basic app with POST form

--- app.rb
+++ app.rb
@@ -2,7 +2,33 @@
 # require 'rubygems'
 require 'sinatra'
 
+# home route
 get '/' do 
 	"Welcome to the DMC's Scrabble Solver!"
 end
 
+# test params route
+get '/sayhi/:name' do
+	"Hi #{params[:name]}!"
+end
+
+get '/form' do
+	erb :form
+end
+
+post '/form' do
+	"You said '#{params[:message]}'"
+	"Reversed message is: #{reverse_text(params[:message])}"
+end
+
+not_found do  
+	status 404  
+	'Page not found, sorry!'  
+end  
+
+
+def reverse_text(_txt)
+
+	_txt.reverse
+
+end

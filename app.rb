093c439bbd00e9173316a80fe9542358671d986e
garciadelcastillo <personal@garciadelcastillo.es>
2013-11-29 23:52:30 -0500

    App working, needs cleanup and optimization

--- app.rb
+++ app.rb
@@ -10,9 +10,9 @@ end
 
 # Main POST action
 post '/solve' do
-	@query = reverse_text(params[:letters])
+	@query = params[:letters]
 	@letters = combine_text(params[:letters])
-	# puts @letters
+	@possible_words = recombine_letters(params[:letters])
 	erb :scrabbled
 end
 
@@ -27,6 +27,95 @@ not_found do
 	'Page not found, sorry! XD'  
 end 
 
+
+helpers do
+
+	def reverse_text(_txt)
+		_txt.reverse
+	end
+
+	def combine_text(_txt)
+		s = _txt
+		letters = s.split(//)
+	end
+
+	def recombine_letters(_txt)
+		s = _txt.downcase
+		letters = s.split(//)
+		len = _txt.length
+
+		dict = {}
+		for i in 1..len
+			letters.permutation(i).each do |word|
+				dict[word.join] = true
+			end
+		end
+
+		puts "DEBUG: Generated " + dict.length.to_s + " permutations"
+
+		matches = []
+		File.open('./assets/words.txt').each_line do |line|
+			line.chomp!
+			matches << line if dict[line]
+		end
+
+		scores = {}
+		matches.each do |word|
+			scores[word] = calculate_word_score(word)
+			puts word + ": " + scores[word].to_s + " points"
+		end
+
+		puts scores
+		sorted_scores = scores.sort_by{|k,v|v}.reverse[0..9]
+		puts sorted_scores
+
+		return sorted_scores 
+
+	end
+
+	def calculate_word_score(_word)
+
+		# this should be defined somewhere outside
+		score_hash = {
+			"a" => 1,
+			"b" => 3,
+			"c" => 3,
+			"d" => 2,
+			"e" => 1,
+			"f" => 4,
+			"g" => 2,
+			"h" => 4,
+			"i" => 1,
+			"j" => 8,
+			"k" => 5,
+			"l" => 1,
+			"m" => 3,
+			"n" => 1,
+			"o" => 1,
+			"p" => 3,
+			"q" => 10,
+			"r" => 1,
+			"s" => 1,
+			"t" => 1,
+			"u" => 1,
+			"v" => 4,
+			"w" => 4,
+			"x" => 8,
+			"y" => 4,
+			"z" => 10
+		}
+
+		score = 0
+
+		_word.each_char do |c|
+			score += score_hash[c]
+		end
+		
+		return score
+
+	end
+
+end
 
 
 
--- server/helpers.rb
+++ server/helpers.rb
@@ -1,14 +1,29 @@
-helpers do
 
-	def reverse_text(_txt)
-		_txt.reverse
-	end
+## --> MOVED TO APP.RB because the reloading wasn't working in this file
 
-	def combine_text(_txt)
-		s = _txt
-		letters = s.split(//)
-	end
+# helpers do
 
-end
+# 	def reverse_text(_txt)
+# 		_txt.reverse
+# 	end
+
+# 	def combine_text(_txt)
+# 		s = _txt
+# 		letters = s.split(//)
+# 	end
+
+# 	def recombine_letters(_txt)
+# 		s = _txt
+# 		letters = s.split(//)
+# 		len = letters.length
+# 		for i in 0..len
+# 			for j in 0..len
+# 				puts i * j
+# 			end
+# 		end
+
+# 	end
+
+# end
 
 
